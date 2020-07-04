dc5ef169981b4dfa5fe978bd01a0bb3cd1c9dd0f
alphadzingina <alphaadx6@gmail.com>
2020-07-04 14:41:42 +0100

    render number of  search results found

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,6 +4,7 @@ import MovieCard from './MovieCard';
 const MovieList = (props) => {
     return (
         <div className="card-list">
+            found {props.movies.length} results <br />
             {props.movies.filter(movie => movie.poster_path).map((movie) => <MovieCard movie={movie} key={movie.id}/>)}
         </div>
     );
