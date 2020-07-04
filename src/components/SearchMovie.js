b59b478ef1dcdb9b505ce09e3d643b327de2c662
alphadzingina <alphaadx6@gmail.com>
2020-07-04 01:40:11 +0100

    use state to update search term and results

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const SearchMovie = () => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResult, setSearchResult] = useState([]);
+
     const fetchMovies = async (e) => {
         e.preventDefault();
-        console.log("submitting");
 
-        const query = "The Irishman";
-
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=655b7759e480c5448f597d5535f76938&query=${query}&page=1&include_adult=false`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=655b7759e480c5448f597d5535f76938&query=${searchTerm}&page=1&include_adult=false`;
 
         try {
             const res = await fetch(url);
             const data  = await res.json();
-            console.log(data);
+            setSearchResult(data.results)
         }catch(err){
             console.error(err);
         }
@@ -21,7 +22,14 @@ const SearchMovie = () => {
         <div>
             <form className="form" onSubmit={fetchMovies}>
                 <label className="label" htmlFor="query">Movie name</label>
-                <input className="input" type="text" name="query" placeholder="i.e. The Irishman"/>
+                <input 
+                    className="input" 
+                    type="text" 
+                    name="query" 
+                    placeholder="i.e. The Irishman" 
+                    value={searchTerm} 
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
                 <button className="button" type="submit">Search</button>
             </form>
         </div>
