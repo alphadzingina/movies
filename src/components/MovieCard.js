e04c9fe5f3d331d1fa262adfe0ba0d2cd04e6855
alphadzingina <alphaadx6@gmail.com>
2020-07-04 04:02:30 +0100

    style movie card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,20 +2,22 @@ import React from 'react';
 
 const MovieCard = (props) => {
     return (
-        <div className="card">
-            <img className="card--image"
-                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${props.movie.poster_path}`}
-                alt={props.movie.title + ' poster'}
-            />
-            <div className="card--content">
-                <h3 className="card--title">{props.movie.title}</h3>
-                <p>
-                    <small>RELEASE DATE: {props.movie.release_date}</small>
-                </p>
-                <p>
-                    <small>RATING: {props.movie.vote_average}</small>
-                </p>
-                <p className="card--desc">{props.movie.overview}</p>
+        <div className="ui items movie-card">
+            <div className="item">
+                <div className="image card--image">
+                    <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${props.movie.poster_path}`} alt={props.movie.title + ' poster'} /></div>
+                <div className="content">
+                    <h3 className="header">{props.movie.title}</h3>
+                    <div className="meta">
+                        <span>RELEASE DATE: {props.movie.release_date}</span>
+                    </div>
+                    <div className="extra">
+                        {props.movie.vote_average}
+                    </div>
+                    <div className="description">
+                       <p>{props.movie.overview}</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
